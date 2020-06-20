8fc8f68a828fce828b680f90435d56ff031cac87
Matt Bise <mbise1993@gmail.com>
2020-06-20 14:37:12 -0500

    Remove commented code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-// document.addEventListener('load', () => {
-//   const root = document.querySelector('#root');
-//   root?.append('Hello world!');
-// });
-
 const root = document.querySelector('#root');
 root?.append('Hello world!');
